64aa310989af3158704b5b9e768ac41cab9e489c
Hemant Sah <hemantsah2912@gmail.com>
2026-03-24 22:18:08 +0530

    add updateNote

--- backend/src/controllers/note.controller.js
+++ backend/src/controllers/note.controller.js
@@ -10,7 +10,7 @@ const createNote = asyncHandler(async (req, res) =>{
     // createNote
 });
 const updateNote = asyncHandler(async (req, res) =>{
-
+    // updateNote
 });
 const deleteNote = asyncHandler(async (req, res) =>{
 
