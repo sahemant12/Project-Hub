9fb3d45f21d6e18623d0ca839144613b4d1a2ea5
Hemant Sah <hemantsah2912@gmail.com>
2026-04-11 20:39:07 +0530

    remove resetForgottenPassword

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -30,9 +30,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 
 const forgotPasswordRequest = asyncHandler(async (req, res) => {
 });
-const resetForgottenPassword = asyncHandler(async (req, res) => {
-    // resetForgottenPassword
-    // get resetForgottenPassword
+const resetForgottenPassword = asyncHandler(async (req, res) => {re
 });
 const changeCurrentPassword = asyncHandler(async (req, res) => {
     // changeCurrentPassword
