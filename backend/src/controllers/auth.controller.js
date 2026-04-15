b21e2acfaa52414698bb6b2d3698b21806cc1a40
Hemant Sah <hemantsah2912@gmail.com>
2026-04-15 18:47:48 +0530

    remove resendEmailVerification

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -8,7 +8,6 @@ const verifyEmail = asyncHandler(async (req, res) => {
 });
 
 const resendEmailVerification = asyncHandler(async (req, res) => {
-    // resendEmailVerification
 });
 
 const loginUser = asyncHandler(async (req, res) => {
